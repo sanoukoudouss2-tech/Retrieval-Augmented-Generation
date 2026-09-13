623d4f557a2c4d4f4bbd4074d271d92a2f0067c3
sanoukoudouss2-tech <sanoukoudouss2@gmail.com>
2026-09-13 22:01:19 +0100

    Delete src directory

--- src/prompt.py
+++
@@ -1,4 +0,0 @@
-def prompt(question,contexte):
-    A = {"role":"system","content":"tu reponds uniquement à partir du contexte fourni. Si le contexte est insuffisant, dis le clairement"}
-    B= {"role":"user","content": f""" Contexte : {contexte} \n Question : {question}"""}
-    return [A,B]
